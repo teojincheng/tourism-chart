888408a0dae8776e49daf9c8cfb66743ea62e552
teojincheng <jinchengproject@gmail.com>
2020-04-25 21:11:34 +0800

    [jc] update attempt of file. upload reference of proper chart code with x axis as categories

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var chart = c3.generate({
   bindto: "#chart",
   data: {
-    columns: [
-      ["data1", 30, 200, 100, 400, 150, 250],
-      ["data2", 50, 20, 10, 40, 15, 25],
-    ],
+    url: "/tour_data.json",
+    type: "line",
+    axis: {
+      x: {
+        type: "categorized",
+      },
+    },
   },
 });
